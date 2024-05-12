4d3463d3e284ff69b12a247eee8a426369ac05be
sldev2 <slarres3@gmail.com>
2024-05-12 16:07:43 -0400

    Add project files.

---
+++ BlazorWithIdentityDO/BlazorWithIdentityDO.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWithIdentityDO.Client;
+using BlazorWithIdentityDO.Client.Services.Contracts;
+using BlazorWithIdentityDO.Client.Services.Implementations;
+using BlazorWithIdentityDO.Client.States;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ BlazorWithIdentityDO/BlazorWithIdentityDO.Server/Controllers/SampleDataController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorWithIdentityDO.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWithIdentityDO.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class SampleDataController : Controller
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> WeatherForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+    }
+}
